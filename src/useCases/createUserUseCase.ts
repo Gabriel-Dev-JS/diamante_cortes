1f2d5aaba1cc5c1111386f91f20b15207331f61a
Gabriel Oliveira <gabrieladscamp@gmail.com>
2026-01-08 11:51:44 -0300

    arrumando identação

--- src/useCases/createUserUseCase.ts
+++ src/useCases/createUserUseCase.ts
@@ -8,28 +8,27 @@ interface PropsBody{
 
 class CreateUserUseCase {
   cadastrarUsuario(data:PropsBody) {
-
-        const [usuario] = user.filter(val => val.email == data?.email || val.senha == data?.senha)
-        console.log("usuario: ", usuario)
-        if(usuario?.email || usuario?.senha){
-          throw new Error()
-        }
-
-        const userIds = user.map(user => user)
-        const idUsers = userIds[userIds.length - 1]
-
-        const users = user.push(
-           {
-            "id":  Number(idUsers?.id) + 1,
-            "nome": data?.nome,
-            "email": data?.email,
-            "senha": data?.senha
-            }
-        )
-
-        console.log("users: ", users)
-        return users
+    const [usuario] = user.filter(val => val.email == data?.email || val.senha == data?.senha)
+    console.log("usuario: ", usuario)
+    if(usuario?.email || usuario?.senha){
+      throw new Error()
     }
+
+    const userIds = user.map(user => user)
+    const idUsers = userIds[userIds.length - 1]
+
+    const users = user.push(
+        {
+        "id":  Number(idUsers?.id) + 1,
+        "nome": data?.nome,
+        "email": data?.email,
+        "senha": data?.senha
+        }
+    )
+
+    console.log("users: ", users)
+    return users
+  }
 }
 
 // export default new CreateUserUseCase;
